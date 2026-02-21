cba9d596185a44e1fac30bee8bb6c80cdb2247b8
Aigiz Tulubaev <flaylay256@gmail.com>
2026-02-21 19:34:32 +0300

    feat: add ui kit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,22 @@ import type { Metadata } from 'next'
 import './globals.scss'
 import { Providers } from '@/providers'
 import { AppLayout } from '@/widgets/layout/AppLayout'
+import { Inter, Montserrat } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Spotify',
   description: 'Сервис для прослушивания музыки',
 }
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+})
+
+const montserrat = Montserrat({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+})
 
 export default function RootLayout({
   children,
@@ -15,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body>
+      <body className={inter.className || montserrat.className}>
         <Providers>
           <AppLayout>{children}</AppLayout>
         </Providers>
--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -4,8 +4,27 @@ import Link from 'next/link'
 import { Home, Library, Search } from 'lucide-react'
 import { Logo } from '@/shared/Logo'
 import { Avatar } from '@/shared/Avatar'
+import { ProfileSidebar } from '@/shared/ProfileSidebar'
 
 const Sidebar = () => {
+  const sidebarLinks = [
+    {
+      name: 'Поиск',
+      icon: Search,
+      href: '/',
+    },
+    {
+      name: 'Главная',
+      icon: Home,
+      href: '/search',
+    },
+    {
+      name: 'Библиотека',
+      icon: Library,
+      href: '/library',
+    },
+  ]
+
   return (
     <aside className={styles.sidebar}>
       <div className={styles.brand}>
@@ -13,39 +32,19 @@ const Sidebar = () => {
       </div>
       <nav className={styles.nav} aria-label="Основная навигация">
         <ul className={styles['nav-list']}>
-          <li className={styles['nav-item']}>
-            <Link className={styles['nav-link']} href="#">
-              <span className={styles['nav-icon']} aria-hidden>
-                <Search className={styles['nav-icon-svg']} />
-              </span>
-              Поиск
-            </Link>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link className={styles['nav-link']} href="#">
-              <span className={styles['nav-icon']} aria-hidden>
-                <Home className={styles['nav-icon-svg']} />
-              </span>
-              Главная
-            </Link>
-          </li>
-          <li className={styles['nav-item']}>
-            <Link className={styles['nav-link']} href="#">
-              <span className={styles['nav-icon']} aria-hidden>
-                <Library className={styles['nav-icon-svg']} />
-              </span>
-              Библиотека
-            </Link>
-          </li>
+          {sidebarLinks.map((elem, index) => (
+            <li key={index} className={styles['nav-item']}>
+              <Link className={styles['nav-link']} href="#">
+                <span className={styles['nav-icon']} aria-hidden>
+                  <elem.icon size={22} />
+                </span>
+                {elem.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
-      <div className={styles['profile']}>
-        <Avatar />
-        <div className={styles['profile-name']}>
-          <span>Тулыбаев Айгиз</span>
-          <div className={styles['profile-status']}>Trial</div>
-        </div>
-      </div>
+      <ProfileSidebar />
     </aside>
   )
 }
